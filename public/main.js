8336276c937606a069a1aa1ab6341fa5f54291e3
Jenny Sadler <jennysadler@Jennys-MBP.broadband>
2020-11-12 21:55:44 +0000

    User can now search for a city and weather and coords are shown. Styling updated

--- public/main.js
+++ public/main.js
@@ -2,13 +2,13 @@
 startUp();
 
 async function startUp(){
+    document.getElementById("location").textContent = "Current Location"
     navigator.geolocation.getCurrentPosition(async position => {
     const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    // console.log(lat, long);
-    // document.getElementById("lat").textContent = lat;
-    // document.getElementById("long").textContent = long;
-    const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&&units=metric&appid=ccd0635e04093f8a9f29ccafa866df0e`
+    const long = position.coords.longitude;
+    document.getElementById("lat").textContent = lat;
+    document.getElementById("long").textContent = long;
+    const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&&units=metric&appid=ccd0635e04093f8a9f29ccafa866df0e`
     fetch(api_url).then(async (response) => {
         const data = await response.json();
         const temp = data.main.temp;
@@ -17,37 +17,60 @@ async function startUp(){
         document.getElementById("temp").textContent = temp  + " degrees celsius";;
         document.getElementById("feels_like").textContent = feels_like  + " degrees celsius";;
         document.getElementById("description").textContent = description;
+        if(description.includes("rain")){
+            console.log("rain")
+            document.getElementById("weathericon").src="rain.svg";
+        } else if(description.includes("cloud")){
+            console.log("cloud")
+            document.getElementById("weathericon").src="cloud.svg";
+        } else if(description.includes("sun")){
+            console.log("sun")
+            document.getElementById("weathericon").src="sun.svg";
+        } else if(description.includes("thunder")){
+            console.log("cloud")
+            document.getElementById("weathericon").src="thunder.svg";
+        } else if(description.includes("snow")){
+            console.log("cloud")
+            document.getElementById("weathericon").src="snowy.svg";
+        }
       })
     });
 }
 
-// async function getWeather(city){
-// console.log(city)
-//     const api_url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=ccd0635e04093f8a9f29ccafa866df0e`
-//     fetch(api_url).then(async (response) => {
-        // const data = await response.json();
-        // const temp = data.main.temp;
-        // const feels_like = data.main.feels_like;
-        // const description = data.weather[0].description;
-        // document.getElementById("temp").textContent = temp  + " degrees celsius";;
-        // document.getElementById("feels_like").textContent = feels_like + " degrees celsius";
-        // document.getElementById("description").textContent = description;
-        
-//       })
-//     };
 
     async function getWeather(){
         const city = document.getElementById("cityInput").value
-        console.log(city)
+        document.getElementById("location").textContent = city
+        
         const api_url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=ccd0635e04093f8a9f29ccafa866df0e`
         fetch(api_url).then(async (response) => {
             const data = await response.json();
+            const lat = data.coord.lat;
+            const long = data.coord.lon;
+            document.getElementById("lat").textContent = lat;
+            document.getElementById("long").textContent = long;
             const temp = data.main.temp;
             const feels_like = data.main.feels_like;
             const description = data.weather[0].description;
             document.getElementById("temp").textContent = temp  + " degrees celsius";;
             document.getElementById("feels_like").textContent = feels_like + " degrees celsius";
             document.getElementById("description").textContent = description;
+            if(description.includes("rain")){
+                console.log("rain")
+                document.getElementById("weathericon").src="rain.svg";
+            } else if(description.includes("cloud")){
+                console.log("cloud")
+                document.getElementById("weathericon").src="cloud.svg";
+            } else if(description.includes("sun")){
+                console.log("sun")
+                document.getElementById("weathericon").src="sun.svg";
+            } else if(description.includes("thunder")){
+                console.log("cloud")
+                document.getElementById("weathericon").src="thunder.svg";
+            } else if(description.includes("snow")){
+                console.log("cloud")
+                document.getElementById("weathericon").src="snowy.svg";
+            }
                 
               })
             };
